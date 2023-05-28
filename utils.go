a8e561592b2b99ad17601c7a1a8ee4e31b5c9b77
Khadem <a.khadem@digikala.com>
2023-05-28 21:54:09 +0330

    feat: Write some test for validations

--- utils.go
+++ utils.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net"
+	"testing"
+	"time"
 
 	pb "github.com/my/repo/grpc"
 
@@ -15,6 +18,33 @@ func loadEnv() {
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
 	}
+}
+
+func AssertErrorMessagesEqual(
+	t testing.TB,
+	expected error,
+	actual error,
+) {
+	if (expected == nil && actual != nil) || (expected != nil && actual == nil) {
+		t.Errorf("Expected error message %v but got %v", expected, actual)
+	} else if expected != nil && actual != nil {
+		if expected.Error() != actual.Error() {
+			t.Errorf("Expected error message '%s' but got '%s'", expected.Error(), actual.Error())
+		}
+	}
+}
+
+func RandomString(length int) string {
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[r.Intn(len(charset))]
+	}
+
+	return string(b)
 }
 
 func startServer() {
---
+++ validations_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+)
+
+func TestCheckNonce(t *testing.T) {
+	tests := []struct {
+		name  string
+		nonce string
+		err   error
+	}{
+		{
+			name:  "valid nonce",
+			nonce: RandomString(NonceLength),
+			err:   nil,
+		},
+		{
+			name:  "empty nonce",
+			nonce: "",
+			err: status.Errorf(
+				codes.InvalidArgument,
+				"Nonce length must be %d",
+				NonceLength,
+			),
+		},
+		{
+			name:  "nonce too short",
+			nonce: "abc",
+			err: status.Errorf(
+				codes.InvalidArgument,
+				"Nonce length must be %d",
+				NonceLength,
+			),
+		},
+		{
+			name:  "nonce too long",
+			nonce: "abcdefghijklmnopqrstuvwxyz",
+			err: status.Errorf(
+				codes.InvalidArgument,
+				"Nonce length must be %d",
+				NonceLength,
+			),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := checkNonce(tt.nonce)
+			AssertErrorMessagesEqual(t, err, tt.err)
+		})
+	}
+}
+
+func TestIsMessageIdValid(t *testing.T) {
+	testCases := []struct {
+		messageId uint64
+		expected  bool
+	}{
+		{messageId: 4, expected: true},
+		{messageId: 5, expected: false},
+		{messageId: 0, expected: true},
+	}
+
+	for _, tc := range testCases {
+		if output := isMessageIdValid(tc.messageId); output != tc.expected {
+			t.Errorf(
+				"Expected isMessageIdValid(%d) to be %v, got %v",
+				tc.messageId,
+				tc.expected,
+				output,
+			)
+		}
+	}
+}
